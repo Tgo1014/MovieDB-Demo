eaf6e7b7d077d4a325c253d77618098dce838455
Tiago Araujo <taraujodesouza@gmail.com>
2018-03-29 16:14:06 -0300

    shared element transition android L+

--- app/src/main/java/tgo1014/moviedb_demo/ui/MovieDetailsActivity.java
+++ app/src/main/java/tgo1014/moviedb_demo/ui/MovieDetailsActivity.java
@@ -1,20 +1,31 @@
 package tgo1014.moviedb_demo.ui;
 
+import android.app.Activity;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 
 import tgo1014.moviedb_demo.R;
 import tgo1014.moviedb_demo.entities.Movie;
@@ -24,6 +35,7 @@ import tgo1014.moviedb_demo.viewmodels.MoviesViewModel;
 public class MovieDetailsActivity extends AppCompatActivity {
 
     private static final String MOVIE_ID = "MOVIE_ID";
+    private static final String MOVIE_TRANSITION_NAME = "MOVIE_TRANSITION_NAME";
     int movieId;
 
     private MoviesViewModel moviesVM;
@@ -43,9 +55,6 @@ public class MovieDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
 
-        Intent intent = this.getIntent();
-        movieId = intent.getIntExtra(MOVIE_ID, 0);
-
         detailActivityTvTitle = findViewById(R.id.detail_activity_tv_title);
         detailActivityTvTagline = findViewById(R.id.detail_activity_tv_tagline);
         detailActivityIvHeader = findViewById(R.id.detail_activity_iv_header);
@@ -59,9 +68,16 @@ public class MovieDetailsActivity extends AppCompatActivity {
         setSupportActionBar(detailActivityToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        supportPostponeEnterTransition();
+        Intent intent = getIntent();
+        movieId = intent.getIntExtra(MOVIE_ID, 0);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            String imageTransitionNam2e = intent.getStringExtra(MOVIE_TRANSITION_NAME);
+            detailActivityIvPoster.setTransitionName(imageTransitionNam2e);
+        }
+
         moviesVM = ViewModelProviders.of(this).get(MoviesViewModel.class);
         subscribeToVM(moviesVM);
-
     }
 
     @Override
@@ -115,9 +131,23 @@ public class MovieDetailsActivity extends AppCompatActivity {
                     .apply(new RequestOptions().placeholder(R.drawable.ic_movie_placeholder).centerCrop())
                     .apply(new RequestOptions().centerCrop())
                     .into(detailActivityIvHeader);
+
             Glide.with(this)
                     .load(RestClient.BASE_URL_POSTER_SIZE_342 + movie.getPosterPath())
                     .apply(new RequestOptions().placeholder(R.drawable.ic_movie_placeholder).centerCrop())
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            supportStartPostponedEnterTransition();
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            supportStartPostponedEnterTransition();
+                            return false;
+                        }
+                    })
                     .into(detailActivityIvPoster);
         }
     }
@@ -126,5 +156,16 @@ public class MovieDetailsActivity extends AppCompatActivity {
         Intent intent = new Intent(context, MovieDetailsActivity.class);
         intent.putExtra(MOVIE_ID, movieId);
         context.startActivity(intent);
+    }
+
+    public static void startWithAnimation(Context context, Activity activity, int movieId, View sharedImageView) {
+        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                activity,
+                sharedImageView,
+                ViewCompat.getTransitionName(sharedImageView));
+        Intent intent = new Intent(context, MovieDetailsActivity.class);
+        intent.putExtra(MOVIE_ID, movieId);
+        intent.putExtra(MOVIE_TRANSITION_NAME, ViewCompat.getTransitionName(sharedImageView));
+        context.startActivity(intent, options.toBundle());
     }
 }
